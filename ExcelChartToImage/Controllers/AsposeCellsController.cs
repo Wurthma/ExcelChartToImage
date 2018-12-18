1a1f5c4b2b28d7801703487ac1fb8648e2b45b4e
Wurthmann <ge_h_w@hotmail.com>
2018-12-17 23:12:17 -0200

    Commint

---
+++ ExcelChartToImage/Controllers/AsposeCellsController.cs
@@ -0,0 +1,40 @@
+﻿using Aspose.Cells;
+using ExcelChartToImage.Models;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ExcelChartToImage.Controllers
+{
+    public class AsposeCellsController : Controller
+    {
+        // GET: AsposeCells
+        public ActionResult Index()
+        {
+            HomeModel model = new HomeModel();
+            model.ListaExcelChartImg = new List<byte[]>();
+            //Pegar o caminho do projeto
+            string path = Server.MapPath("~");
+            //Abrir arquivo excel com Aspose Cells
+            Workbook workbook = new Workbook(path + "\\Content\\column-chart.xlsx");
+            Worksheet worksheet = workbook.Worksheets[0];
+
+            foreach (var grafico in worksheet.Charts)
+            {
+                MemoryStream ms = new MemoryStream();
+                grafico.ToImage().Save(ms, ImageFormat.Jpeg);
+                byte[] bmpBytes = ms.ToArray();
+                model.ListaExcelChartImg.Add(bmpBytes);
+
+            }
+
+            return View(model);
+        }
+    }
+}
